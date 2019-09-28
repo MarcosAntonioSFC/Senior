4dd7f06f1b8ee7e83a45289ab368f14cbf6f11ca
Marcos Antônio <marcos.antonio@cirruserp.com.br>
2019-09-28 13:56:03 -0300

    Senior - Estruturação - Repensando a estrutura do service

--- src/main/java/br/com/senior/controller/abstracts/Service.java
+++ src/main/java/br/com/senior/controller/abstracts/Service.java
@@ -2,21 +2,11 @@ package br.com.senior.controller.abstracts;
 
 import br.com.senior.model.abstracts.AbstractModel;
 
-import org.springframework.data.domain.Page;
-
 /**
  * Interface para disponibilizar os métodos do crud.
  *
  * @param <T> model com extensão.
  */
 public interface Service<T extends AbstractModel> {
-
-  Class<T> getEntityClass();
-
-  T create(T model) throws ServiceException;
-
-  T getById(final String id) throws RowNotFoundException, ServiceException;
-
-  Page<T> getAll(Integer page, Integer pageSize) throws ServiceException;
 
 }
